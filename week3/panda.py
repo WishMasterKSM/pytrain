1e40bff6905c33c83ebad68701a1894ec6227ab9
WishMasterKSM <Vasil.Yushchenko@gmail.com>
2020-02-10 12:36:10 +0200

    missing tasks

--- week3/panda.py
+++ week3/panda.py
@@ -50,6 +50,34 @@ def task4(df):
 def task5(df):
     print(df['company'].value_counts())
 
+@task_divider
+def task6(df):
+    print(df.groupby('company')['price'].max())
+
+@task_divider
+def task7(df):
+    print(df.groupby('company')['average-mileage'].mean())
+
+@task_divider
+def task8(df):
+    print(df.sort_values(by=['price']))
+
+@task_divider
+def task9(df):
+    GermanCars = {'Company': ['Ford', 'Mercedes', 'BMV', 'Audi'], 'Price': [23845, 171995, 135925 , 71400]}
+    JapaneseCars = {'Company': ['Toyota', 'Honda', 'Nissan', 'Mitsubishi'], 'Price': [29995, 23600, 61500 , 58900]}
+    df1 = pd.DataFrame.from_dict(GermanCars)
+    df2 = pd.DataFrame.from_dict(JapaneseCars)
+    print(pd.concat([df1, df2], keys=["Germany", "Japan"]))
+
+@task_divider
+def task10(df):
+    Car_Price = {'Company': ['Toyota', 'Honda', 'BMV', 'Audi'], 'Price': [23845, 17995, 135925 , 71400]}
+    Car_Horsepower = {'Company': ['Toyota', 'Honda', 'BMV', 'Audi'], 'horsepower': [141, 80, 182 , 160]}
+    df1 = pd.DataFrame.from_dict(Car_Price)
+    df2 = pd.DataFrame.from_dict(Car_Horsepower)
+    print(pd.merge(df1, df2, on="Company"))
+
 def main():
     df = pd.read_csv('Automobile_data.csv', index_col=0)
     task1(df)
@@ -57,6 +85,11 @@ def main():
     task3(df)
     task4(df)
     task5(df)
+    task6(df)
+    task7(df)
+    task8(df)
+    task9(df)
+    task10(df)
 
 if __name__ == '__main__':
     main()
